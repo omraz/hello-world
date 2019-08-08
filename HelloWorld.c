d0d9d5a2ae355fc8473340c64b28f9a8eaee4d8f
omraz <o.mraz@volny.cz>
2019-08-08 11:59:02 +0200

    Added argument list

--- HelloWorld.c
+++ HelloWorld.c
@@ -3,5 +3,8 @@
 int main(int argc, char *argv[])  {
 	printf("Hello, World!\n");
 	printf("arguments: %d\n", argc);
+	for (int i = 0; i < argc; i++) {
+		printf("%3d\t%s\n", i, argv[i]);
+	}
 	return 0;
 }

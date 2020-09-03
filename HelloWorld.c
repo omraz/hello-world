27520355f08120bb4eceb0479c68499d769ecf12
omraz <o.mraz@volny.cz>
2020-09-03 09:07:45 +0200

    global changes

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 
 int main(int argc, char *argv[])  {
-	printf("Hello, World!\n");
+	printf("Hello, World!\n  from GitHub\n");
 	printf("arguments: %d\n", argc);
 	for (int i = 0; i < argc; i++) {
 		printf("%3d\t%s\n", i, argv[i]);
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 first use test
 
 Tady pridavam novy text a provadim dalsi zmeny, jako treba smazu predchozi nesmysly.
+change on mate-18.04
